260b080aad8950628a0846bf14409549ebc7a8e2
Hamada <hamada.saleh@institutlouisbachelier.org>
2026-02-10 15:56:10 +0100

    Modif pangeo tos no vertex data

--- src/data/cmip6_pangeo.py
+++ src/data/cmip6_pangeo.py
@@ -16,9 +16,15 @@ from pathlib import Path
 
 
 def get_cmip6_data_from_pangeo_api(
-    source_id: str, experiment_id: str, store_dir: Path, institution_id: str, member_id: str
+    source_id: str,
+    experiment_id: str,
+    store_dir: Path,
+    institution_id: str,
+    member_id: str,
 ) -> None:
-    query_list = generate_cmip6_queries(source_id, experiment_id, tbl_var, institution_id, member_id)
+    query_list = generate_cmip6_queries(
+        source_id, experiment_id, tbl_var, institution_id, member_id
+    )
     queries_pbar = tqdm(query_list, desc="Processing pangeo queries")
     for query in queries_pbar:
         queries_pbar.set_postfix(climate_var=query["variable_id"])
@@ -36,7 +42,9 @@ def get_cmip6_data_from_pangeo_api(
                 clim_pbar.set_postfix({"cmip6_setting": clim_key})
                 ds_clim = clim_dset_dict[clim_key].squeeze()
                 # Fetch data up to year 2100 (some models go to year 2300)
-                ds_clim = ds_clim.sel(time=slice("1950-01-01T00:00:00", "2100-12-31T00:00:00"))
+                ds_clim = ds_clim.sel(
+                    time=slice("1950-01-01T00:00:00", "2100-12-31T00:00:00")
+                )
                 if query["variable_id"] == "ta":
                     # Select tropopause air temperature and relative humidity for given pressure levels
                     # TODO: change plev to 5000.0
@@ -44,25 +52,34 @@ def get_cmip6_data_from_pangeo_api(
                 elif query["variable_id"] == "hur":
                     ds_clim["hur"] = ds_clim["hur"].sel(plev=1000.0, method="nearest")
                 elif query["variable_id"] == "hurs":
-                    ds_clim["hurs"] = ds_clim["hurs"].clip(0.,100.)
+                    ds_clim["hurs"] = ds_clim["hurs"].clip(0.0, 100.0)
 
                 # Drop unnecessary dims and variables after selection
-                ds_clim = (
-                    ds_clim.drop_vars(
-                        ["member_id", "dcpp_init_year", "lon_bounds", "lat_bounds"]
-                    )
-                    .reset_coords(["time_bounds", "lat_verticies", "lon_verticies"])
-                    .rename(
+                # Some datasets do not contain "lat_bounds", "lon_bounds"
+                vars_to_drop = [
+                    "member_id",
+                    "dcpp_init_year",
+                    "lon_bounds",
+                    "lat_bounds",
+                ]
+                vars_to_drop = set(ds_clim.variables).intersection(vars_to_drop)
+                coords_to_reset = ["time_bounds", "lat_verticies", "lon_verticies"]
+                coords_to_reset = set(ds_clim.coords).intersection(coords_to_reset)
+                ds_clim = ds_clim.drop_vars(vars_to_drop).reset_coords(coords_to_reset)
+                if set(["lat_verticies", "lon_verticies"]).issubset(ds_clim.coords):
+                    ds_clim = ds_clim.rename(
                         {
                             "lat_verticies": "lat_vertices",
                             "lon_verticies": "lon_vertices",
                         }
                     )
-                )
                 if "plev" in ds_clim.dims:
                     ds_clim = ds_clim.drop_dims("plev")
                 if "plev_bnds" in ds_clim.variables:
                     ds_clim = ds_clim.drop_vars("plev_bnds")
+
+                has_vertices = "vertex" in ds_clim.dims
+                method = "conservative"  # default
 
                 if query["table_id"] == "Amon":
                     ds_clim["lon_vertices"] = ds_clim["lon_vertices"].chunk(
@@ -88,30 +105,21 @@ def get_cmip6_data_from_pangeo_api(
                     ds_clim["lat"].attrs["bounds"] = "lat_b"
                     print(ds_clim.cf)
                 elif query["table_id"] == "Omon":
-                    # Assign cf-compliant bounds for regridding
-                    # For some reason, bounds attributes are dropped after regridding
-                    # Reset them if regridding
-                    ds_clim["lat"].attrs["bounds"] = "lat_vertices"
-                    ds_clim["lon"].attrs["bounds"] = "lon_vertices"
+                    if has_vertices:
+                        method = "conservative"
+                        # FIXME: correct workflow for variables with vertex data
+                        # Assign cf-compliant bounds for regridding
+                        # For some reason, bounds attributes are dropped after regridding
+                        # Reset them if regridding
+                        ds_clim["lat"].attrs["bounds"] = "lat_vertices"
+                        ds_clim["lon"].attrs["bounds"] = "lon_vertices"
+                    # TODO: handle case with no vertices
+                    else:
+                        method = "bilinear"
 
-                # Define an output grid (0.25° lat x 0.25° lon)
-                ds_out = xe.util.grid_global(0.25, 0.25)
-
-                # Print output grid
-                print(ds_out)
-                # Rechunk before regridding, to avoid the entire dataset being loaded into memory
-                # ds_clim = ds_clim.chunk({"time": 10, "y": -1, "x": -1}) # TODO: old chunking strategy seems not to be working anymore
                 var = query["variable_id"]
-                ds_clim[var] = ds_clim[var].chunk({"time": 10, "y": -1, "x": -1})
-                # Create regridder (using first-order conservative method)
-                regridder = xe.Regridder(
-                    ds_clim, ds_out, "conservative", ignore_degenerate=True
-                )
-                # TODO: check NA values for sea level features
+                ds_out = regrid_to_global(ds_clim, var, method)
 
-                ds_out = regridder(ds_clim)
-                # TODO: check this is ok to remove
-                # ds_out = ds_out.drop_vars(["lat_vertices", "lon_vertices"])
                 # Rechunk on time dimension before storing to zarr
                 ds_out = ds_out.chunk({"time": 1, "y": -1, "x": -1})
                 source_id = clim_key.split(".")[2]
@@ -119,8 +127,51 @@ def get_cmip6_data_from_pangeo_api(
                 ds_out.to_zarr(
                     Path(store_dir) / f"{source_id}/{experiment_id}",
                     mode="a",
-                    zarr_format=2
+                    zarr_format=2,
                 )
+
+
+def regrid_to_global(
+    ds_clim: xr.Dataset, var: str, method, resolution=0.25
+) -> xr.Dataset:
+    """
+    Regrid a climate dataset to a global lat/lon grid.
+
+    Parameters
+    ----------
+    ds_clim : xarray.Dataset
+        Input dataset to be regridded.
+    query : dict
+        Dictionary containing at least the key "variable_id".
+    method : str
+        Regridding method (e.g. "conservative", "bilinear").
+    resolution : float, optional
+        Grid resolution in degrees (default is 0.25).
+
+    Returns
+    -------
+    ds_out : xarray.Dataset
+        Regridded dataset.
+    """
+
+    # Define output grid (resolution° lat x resolution° lon)
+    ds_out = xe.util.grid_global(resolution, resolution)
+
+    # Rechunk before regridding to control memory usage
+    ds_clim[var] = ds_clim[var].chunk({"time": 10, "y": -1, "x": -1})
+
+    # Create regridder
+    regridder = xe.Regridder(
+        ds_clim,
+        ds_out,
+        method,
+        ignore_degenerate=True,
+    )
+
+    # Apply regridding
+    ds_out = regridder(ds_clim)
+
+    return ds_out
 
 
 def generate_cmip6_queries(
@@ -164,11 +215,9 @@ def generate_cmip6_queries(
                 "variable_id": variable,
                 "member_id": member_id,
                 "grid_label": grid_label,
-                "institution_id": institution_id
+                "institution_id": institution_id,
             }
             query_list.append(query)
-
-
 
     # Display a sample query for verification
     if query_list:
@@ -186,11 +235,13 @@ if __name__ == "__main__":
     # webbrowser.open(client.dashboard_link)
 
     logger.info(f"Fetching CMIP6 climate data from pangeo API")
-    logger.info(f"Catalog available at this url: {'https://storage.googleapis.com/cmip6/pangeo-cmip6.csv'}")
+    logger.info(
+        f"Catalog available at this url: {'https://storage.googleapis.com/cmip6/pangeo-cmip6.csv'}"
+    )
     url = "https://storage.googleapis.com/cmip6/pangeo-cmip6.json"
     col = intake.open_esm_datastore(url)
     z_kwargs = {"consolidated": True, "decode_times": True, "use_cftime": True}
-    with open("data/input/parameters_for_climate_scenario.json", "r") as f:
+    with open("./parameters_for_climate_scenario.json", "r") as f:
         queries = json.load(f)
     query = queries[4]
     # source_id in emsl refers to model_id in catherina
@@ -201,12 +252,23 @@ if __name__ == "__main__":
     ### TODO lire json et dire que on a remplacé ipsl_cm5a2_inca par ipsl-cm6a-lr
 
     ### CHANGER ICI POUR SCENARIO FUTUR OU HISTORIQUE ###
-    # experiment_id = ["historical"] # pour récupérer les données simulées historiques pour le débiaisage 
+    # experiment_id = ["historical"] # pour récupérer les données simulées historiques pour le débiaisage
 
-    tbl_var = {"Amon": ["hurs", "psl", "ta", ], "Omon": ["tos"]}
+    tbl_var = {
+        "Amon": [
+            "hurs",
+            "psl",
+            "ta",
+        ],
+        "Omon": ["tos"],
+    }
     store_dir = "./data/input/cmip6_data"
     print(f"Save dir: {store_dir}")
     get_cmip6_data_from_pangeo_api(
-        source_id=query["source_id"], experiment_id=query["experiment_id"], store_dir=store_dir, institution_id=query["institution_id"], member_id=query["member_id"]
+        source_id=query["source_id"],
+        experiment_id=query["experiment_id"],
+        store_dir=store_dir,
+        institution_id=query["institution_id"],
+        member_id=query["member_id"],
     )
     print("------------ pangeo done ------------")
